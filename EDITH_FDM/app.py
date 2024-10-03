fea34b82ce069b053b682931433eb4934c0ce45b
Sharumathan <163159851+Sharumathan@users.noreply.github.com>
2024-10-03 16:20:52 +0530

    Update app.py

--- EDITH_FDM/app.py
+++ EDITH_FDM/app.py
@@ -194,7 +194,9 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 4000))  # Default to 4000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
 
 # import time
 # import http.client
